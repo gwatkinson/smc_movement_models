fd451628fd4ce1b9eac3c894fea52f25d9c78434
Jeremstym <jeremstym@gmail.com>
2023-01-02 16:34:57 +0100

    add time windowed sigma

--- smc_movement_models/variance_computing.py
+++ smc_movement_models/variance_computing.py
@@ -5,33 +5,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import acovf
 
-df = pd.read_csv(".../data/clean_data.csv")
+df = pd.read_csv("smc_movement_models/data/clean_data.csv")
 
-acvf = acovf(df.Velocity.values)
+# To build a time window sequenced vector
+# day_str = "18/01/2008"
+day_str_min = "18/01/2008"
+day_str_max = "18/01/2008"
+mask_day = (df["Date"] >= day_str_min) & (df["Date"] <= day_str_max)
+day = df.loc[mask_day, ["Dtime", "Velocity", "Depth"]]
+day["Dtime"] = pd.to_datetime(day["Dtime"])
 
-# Polynomial regression, general framework
+window_size = 312
+overlap_size = int(window_size / 2)
+num_windows = int((len(day) - overlap_size) / overlap_size)
 
-x = np.linspace(0, len(acvf), len(acvf))
-coeffs = np.polyfit(x, acvf, 2)
+windows = []
+ids = []
+for i in range(num_windows):
+    window = day[i * overlap_size : (i + 2) * overlap_size - 1]["Velocity"].values
+    ids.append((i + 1) * overlap_size)
+    windows.append(window)
 
-y = coeffs[0] + coeffs[1] * x + coeffs[2] * x**2
-
-plt.plot(x,y)
-plt.show()
-
+# We need to compute the sigma value for each time window
 # Polynomial regression close to the origin
+sigma_zs = []
+sigma_os = []
+sigma_epsilons = []
+acvf_list = []
 
-x = np.linspace(0, len(acvf[0:9]), len(acvf[0:9]))
-coeffs = np.polyfit(x, acvf[0:9], 2)
+for window in windows:
+    acvf = acovf(window)
+    acvf_list.append(acvf)
 
-def regfit2(k):
-    """
-    k must be an integer (lag)
-    The function is equal to the estimate of sigma_z for all k > 0
-    """
-    return coeffs[0] + coeffs[1] * k + coeffs[2] * k**2
+    x = np.linspace(0, len(acvf[1:9]), len(acvf[1:9]))
+    coeffs = np.polyfit(x, acvf[1:9], 2)
 
-sigma_z0 = regfit2(0)
-sigma_o = acvf[0] - sigma_z0
+    sigma_z = coeffs[2]
+    sigma_o = acvf[0] - sigma_z
+    sigma_epsilon = (1-(acvf[1]/acvf[0])**2)*sigma_z
 
-sigma_epsilon = (1-(acvf[1]/acvf[0])**2)*sigma_z0
+    sigma_zs.append(sigma_z)
+    sigma_os.append(sigma_o)
+    sigma_epsilons.append(sigma_epsilon)
