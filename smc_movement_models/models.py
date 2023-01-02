7fd311969e214d44fc08fc65ba92cc241eb5e27e
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-01-03 00:24:15 +0100

    Add short descriptions to the functions

--- smc_movement_models/models.py
+++ smc_movement_models/models.py
@@ -1,4 +1,4 @@
-"""Module qui implemente les modèles.
+"""Module qui implemente le modèle présenté dans le papier principal.
 
 Examples:
     Exemple d'implementation avec `particles`_:
@@ -21,13 +21,12 @@ Examples:
     https://github.com/nchopin/particles
 """
 
-from particles.distributions import ProbDist, Categorical, StructDist
+from particles.distributions import ProbDist, Categorical
 
 import scipy as sp
 import numpy as np
 
 from particles import SMC
-from particles.smc_samplers import SMC2
 from particles.distributions import Normal, IndepProd
 from particles.state_space_models import Bootstrap, StateSpaceModel
 
@@ -36,7 +35,7 @@ import pandas as pd
 from statsmodels.tsa.stattools import acovf
 
 
-class Mixture(ProbDist):
+class Mixture(ProbDist):  # Copied from the last commit of particles (not yet released).
     """Mixture distributions.
     Parameters
     ----------
@@ -50,7 +49,7 @@ class Mixture(ProbDist):
 
     def __init__(self, pk, *components):
         self.pk = np.atleast_1d(pk)
-        self.k = self.pk.shape[-1]
+        self.k = self.pk.shape[-1]  # Fixed typo
         if len(components) != self.k:
             raise ValueError("Size of pk and nr of components should match")
         self.components = components
@@ -67,6 +66,8 @@ class Mixture(ProbDist):
 
 
 class MarineSSM(StateSpaceModel):
+    """State space model for the seal model."""
+
     default_params = {
         "z0": 0.0,  # Initial value of z_0
         "z1": 0.0,  # Initial value of z_1
@@ -95,7 +96,7 @@ class MarineSSM(StateSpaceModel):
         a2t = Normal(loc=self.a2, scale=self.sigma_v)  # a2_t
         return IndepProd(zt, ztm, a1t, a2t)
 
-    def PX(self, xp):  # dist of X_t at time t, given X_{t-1}
+    def PX(self, t, xp):  # dist of X_t at time t, given X_{t-1}
         loc = xp[:, 2] * xp[:, 0] + xp[:, 3] * xp[:, 1]
         zt = Mixture(  # z_t (mixture)
             [self.c1, self.c2],
@@ -107,51 +108,12 @@ class MarineSSM(StateSpaceModel):
         a2t = Normal(loc=xp[:, 3], scale=self.sigma_v)  # a2_t
         return IndepProd(zt, ztm, a1t, a2t)
 
-    def PY(self, x):  # dist of Y_t at time t, given X_t and X_{t-1}
-        return Normal(loc=x[:, 0], scale=self.sigma_o)
-
-
-class MarineSSM_SMC2(StateSpaceModel):
-    default_params = {
-        "z0": 0.0,  # Initial value of z_0
-        "z1": 0.0,  # Initial value of z_1
-        "a1": 0.0,  # Value of a_1
-        "a2": 0.0,  # Value of a_2
-        "sigma_o": 0.1,  # observation error (y) <-> e
-        "sigma_e": 0.1,  # System noise (z) <-> epsilon
-        "c1": 0.9,  # Mixture options for the system noise
-        "c2": 0.1,
-        "delta": 10,
-    }
-
-    def PX0(self):  # dist of X_0
-        zt = Mixture(  # z_1 (mixture)
-            [self.c1, self.c2],
-            Normal(loc=self.z1, scale=self.sigma_e),
-            Normal(loc=self.z1, scale=self.delta * self.sigma_e),
-        )
-        ztm = Mixture(  # z_0 (mixture)
-            [self.c1, self.c2],
-            Normal(loc=self.z0, scale=self.sigma_e),
-            Normal(loc=self.z0, scale=self.delta * self.sigma_e),
-        )
-        return IndepProd(zt, ztm)
-
-    def PX(self, t, xp):  # dist of X_t at time t, given X_{t-1}
-        loc = self.a1 * xp[:, 0] + self.a2 * xp[:, 1]
-        zt = Mixture(  # z_t (mixture)
-            [self.c1, self.c2],
-            Normal(loc=loc, scale=self.sigma_e),
-            Normal(loc=loc, scale=self.delta * self.sigma_e),
-        )
-        ztm = Normal(loc=xp[:, 0], scale=0.0)  # z_{t-1} (0 variance)
-        return IndepProd(zt, ztm)
-
-    def PY(self, x):  # dist of Y_t at time t, given X_t and X_{t-1}
+    def PY(self, t, xp, x):  # dist of Y_t at time t, given X_t and X_{t-1}
         return Normal(loc=x[:, 0], scale=self.sigma_o)
 
 
 def get_windows(day, overlap_size=156):
+    """Generate overlapping windows of size 2 * overlap_size - 1 from a day of data."""
     num_windows = int((len(day) - overlap_size) / overlap_size)
     windows = []
     window_times = []
@@ -164,6 +126,7 @@ def get_windows(day, overlap_size=156):
 
 
 def estimate_variances(window, nlag=60, n_estimates=8):
+    """Estimate the variances of the observation, system noise for a window."""
     acvf = acovf(window, nlag=nlag)
     x = np.arange(n_estimates + 1)
     y = acvf[: n_estimates + 1]
@@ -183,6 +146,8 @@ def estimate_variance_on_all_windows(
     nlag=12,
     n_estimates=8,
 ):
+    """Estimate the variances of the observation, system noise for all
+    windows."""
     sigma_ods = []
     sigma_eds = []
     sigma_zds = []
@@ -208,22 +173,12 @@ def estimate_variance_on_all_windows(
 
 
 def run_smc(window, N=500, **kwargs):
+    """Run the SMC on a window (one iteration of the algorithm)."""
     my_ssm_model = MarineSSM(z0=window[0], z1=window[1], **kwargs)
     my_fk_model = Bootstrap(ssm=my_ssm_model, data=window)
     my_alg = SMC(fk=my_fk_model, N=N, store_history=True)
     my_alg.run()
     return my_alg
-
-
-def run_smc2(window, N=10):
-    global fenetre
-    fenetre = window
-    my_ssm_model = MarineSSM_SMC2
-    my_prior = StructDist({"a1":Normal(scale=0.1), "a2":Normal(scale=0.1)})
-    my_fk_model = SMC2(ssm_cls=my_ssm_model, prior=my_prior, data=window, init_Nx=5, ar_to_increase_Nx=0.1)
-    my_alg = SMC(fk=my_fk_model, N=N, store_history=True)
-    my_alg.run()
-    return my_alg 
 
 
 def estimate_a1_a2_on_window(
@@ -240,6 +195,7 @@ def estimate_a1_a2_on_window(
     c2=0.1,
     delta=10,
 ):
+    """Run the algorithm on a window."""
     final_a1s = []
     final_a2s = []
     for m in range(M):
@@ -285,6 +241,7 @@ def estimate_a1_a2_on_all_windows(
     c2=0.1,
     delta=10,
 ):
+    """Run the algorithm on all windows."""
     time_a1s = []
     time_a2s = []
 
@@ -320,11 +277,30 @@ def estimate_a1_a2_on_all_windows(
 
     return time_results
 
-def estimate_a1_a2_on_window_SMC2(window):
-    alg = run_smc2(window = window)
-    wgts = np.array([w.W for w in alg.hist.wgts])
-    a_temp = np.array([x.theta for x in alg.hist.X])
-    a1s_temp = a_temp['a1']
-    a2s_temp = a_temp['a2']
-    return np.average(a1s_temp, weights=wgts, axis=1), np.average(a2s_temp, weights=wgts, axis=1)
-    
+
+def rerun_algo(
+    windows,
+    window_times,
+    sigma_os,
+    sigma_es,
+    nb_runs=20,
+    **kwargs,
+):
+    results_var = []
+
+    for i in tqdm(range(nb_runs)):
+        time_results = estimate_a1_a2_on_all_windows(
+            windows=windows,
+            window_times=window_times,
+            sigma_os=sigma_os,
+            sigma_es=sigma_es,
+            **kwargs,
+        )
+        results_var.append(time_results)
+
+    final_res = pd.DataFrame()
+    for i, res in enumerate(results_var):
+        res["run"] = i
+        final_res = pd.concat([final_res, res])
+
+    return final_res
